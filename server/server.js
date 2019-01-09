5a28d1789b9c32d0b06bdd6a6e540f93c70988ab
Maasai Sang <jkemboe@gmail.com>
2019-01-09 19:57:18 +0300

    Greet new user alert others

--- server/server.js
+++ server/server.js
@@ -15,8 +15,6 @@ app.use(express.static(publicPath))
 io.on('connection', (socket) => {
     console.log('New user connected')
 
-     
-
     socket.on('createMessage', (message) => {
         console.log('createMessage',message)
         io.emit('newMessage', {
@@ -24,6 +22,24 @@ io.on('connection', (socket) => {
             text: message.text,
             createdAt: new Date().getTime()
         })
+        
+        socket.emit('newMessage', {
+            from:"Admin",
+            text: "Welcome to the Chat App",
+            createdAt: new Date().getTime()
+        })
+
+        socket.broadcast.emit('newMessage', {
+            from: "Admin",
+            text: "New user joined",
+            createdAt: new Date().getTime()
+        })
+        
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // })
     })
     socket.on('disconnect', () => {
         console.log('User was disconnected')
