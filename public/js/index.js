ccccc4a037ea28294da558783369ef93b93aeddb
Maasai Sang <jkemboe@gmail.com>
2019-01-09 18:59:24 +0300

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,7 @@
 var socket = io();
 socket.on('connect',function () {
     console.log('Connected to server')
-
-    socket.emit('createMessage', {
-        from: 'Joseph',
-        text:' Hey, whats up lads'
-    })
+ 
 })
 socket.on('disconnect', function () {
     console.log('Disconnected from server')
