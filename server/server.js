8532db562f50c242ba11934a9c36c5e7343a642d
Maasai Sang <jkemboe@gmail.com>
2019-01-10 19:14:48 +0300

    Add form for messages and show incoming messages

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http')
 const express = require('express')
 const socketIO = require('socket.io')
 
+const {generateMessage} = require('./utils/message')
 var app = express()
 var server = http.createServer(app)
 var io = socketIO(server)
@@ -17,34 +18,26 @@ io.on('connection', (socket) => {
 
     socket.on('createMessage', (message) => {
         console.log('createMessage',message)
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        })
-        
-        socket.emit('newMessage', {
-            from:"Admin",
-            text: "Welcome to the Chat App",
-            createdAt: new Date().getTime()
-        })
+         
+        socket.emit('newMessage', generateMessage('Admin', 'Welcome to the Chat app'))
 
-        socket.broadcast.emit('newMessage', {
-            from: "Admin",
-            text: "New user joined",
-            createdAt: new Date().getTime()
+        socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'))
+
+        socket.on('createMessage', (message, callback) => {
+            console.log('createMessage', message)
+            io.emit('newMessage',generateMessage(message.from, message.text))
+            callback('This is from the server')
         })
-        
         // socket.broadcast.emit('newMessage', {
         //     from: message.from,
         //     text: message.text,
         //     createdAt: new Date().getTime()
         // })
     })
+
     socket.on('disconnect', () => {
         console.log('User was disconnected')
     })
-
 })
 
 server.listen(port, () => {
