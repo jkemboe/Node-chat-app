8dcb4dedabbd5fd938dfaeb461af55a82ec4ad20
Maasai Sang <jkemboe@gmail.com>
2019-01-11 17:02:14 +0300

    Added format timestamps using momentjs

---
+++ playground/time.js
@@ -0,0 +1,12 @@
+var moment = require('moment')
+
+// var date = new Date()
+
+// console.log(date.getTime())
+
+var createdAt = 1234
+
+var someTimeStamp = moment().valueOf()
+var date = moment(createdAt)
+
+console.log(someTimeStamp)
